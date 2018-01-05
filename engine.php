cf1b35bcca7c1ac25d1abccf4d646cf52fb0b8b2
vpervenditti <34209633+vpervenditti@users.noreply.github.com>
2018-01-05 21:33:36 +0100

    Update engine.php

--- engine.php
+++ engine.php
@@ -9,7 +9,9 @@ curl_setopt($c, CURLOPT_HEADER, 0);
 curl_setopt($c, CURLOPT_URL, "https://affiliate.gearbest.com/api/products/" . $method . "?" . $args);	
 
 curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);	
-$r = curl_exec($c);	curl_close($c);	return $r;
+$r = curl_exec($c);	
+curl_close($c);	
+return json_decode($r);
 }
 
 function getCompletedOrders(){ 	
@@ -66,3 +68,35 @@ $args = "api_key=".$api_key."&time=".$time."&lkid="."&type=".$type."&category=".
 return curlRequest("banner/list-event-creative", $args);
 }
 
+
+
+
+function listProductCreative($type, $category) {
+
+global $config;
+$str = "";	
+$args = "";		
+ksort($config);	
+foreach ($config as $key => $val)	{
+$str = $str . $key . $val;
+}	
+$sign = strtoupper(md5($api_secret . $str . $api_secret));		
+$args = "api_key=".$api_key."&time=".$time."&type=".$type."&category=".$category."&page=".$page."&sign=".$sign."&lkid=".$lkid;	
+return curlRequest("promotions/list-product-creative", $args);
+}
+
+
+
+function listCoupons($type, $category) {
+
+global $config;
+$str = "";	
+$args = "";		
+ksort($config);	
+foreach ($config as $key => $val)	{
+$str = $str . $key . $val;
+}	
+$sign = strtoupper(md5($api_secret . $str . $api_secret));		
+$args = "api_key=".$api_key."&time=".$time."&language=".$language."&category=".$category."&page=".$page."&sign=".$sign."&lkid=".$lkid;	
+return curlRequest("coupon/list-coupon", $args);
+}
