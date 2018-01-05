289c7bf96447a48c9ab6e5358e9af08c47aa1c22
vpervenditti <34209633+vpervenditti@users.noreply.github.com>
2018-01-05 21:19:07 +0100

    Update engine.php

--- engine.php
+++ engine.php
@@ -1,48 +1,68 @@
 <?php
-
 $file = file_get_contents("./config.json");
 $config = json_decode($file, true);
 $time = time();
+function curlRequest($method, $args){	
+$c = curl_init();
+curl_setopt($c, CURLOPT_HEADER, 0);	
 
-function curlRequest($method, $args)
-{
-	$c = curl_init();
-	curl_setopt($c, CURLOPT_HEADER, 0);
-	curl_setopt($c, CURLOPT_URL, "https://affiliate.gearbest.com/api/products/" . $method . "?" . $args);
-	curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-	$r = curl_exec($c);
-	curl_close($c);
-	return $r;
+curl_setopt($c, CURLOPT_URL, "https://affiliate.gearbest.com/api/products/" . $method . "?" . $args);	
+
+curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);	
+$r = curl_exec($c);	curl_close($c);	return $r;
 }
 
-
-function getCompletedOrders()
-{ 
-	global $config;
-	$str = "";
-	$args = "";
-	
-	ksort($config);
-	foreach ($config as $key => $val)
-	{
-		$str = $str . $key . $val;
-	}
-	$sign = strtoupper(md5($api_secret . $str . $api_secret));
-	
-	$args = "api_key=".$api_key."&time=".$time."&lkid=".$lkid."&currency=".$currency."&page=".$page."&sign=".$sign;
-
-	return curlRequest("list-promotion-products", $args);
+function getCompletedOrders(){ 	
+global $config;	
+$str = "";	
+$args = "";		
+ksort($config);	
+foreach ($config as $key => $val)	{
+$str = $str . $key . $val;
+}	
+$sign = strtoupper(md5($api_secret . $str . $api_secret));		
+$args = "api_key=".$api_key."&time=".$time."&lkid=".$lkid."&currency=".$currency."&page=".$page."&sign=".$sign;	
+return curlRequest("list-promotion-products", $args);
 }
-
+function listPromotionProduct() {
+global $config;
+$str = "";	
+$args = "";		
+ksort($config);	
+foreach ($config as $key => $val)	{
+$str = $str . $key . $val;
+}	
+$sign = strtoupper(md5($api_secret . $str . $api_secret));		
+$args = "api_key=".$api_key."&time=".$time."&lkid=".$lkid."&currency=".$currency."&page=".$page."&sign=".$sign;	
+return curlRequest("products/list-promotion-products", $args);
+}
 
 function listPromotionProduct() {
-	
-	
-		
-	
-	
-	
-	
-	
-	
-	
+global $config;
+$str = "";	
+$args = "";		
+ksort($config);	
+foreach ($config as $key => $val)	{
+$str = $str . $key . $val;
+}	
+$sign = strtoupper(md5($api_secret . $str . $api_secret));		
+$args = "api_key=".$api_key."&time=".$time."&lkid=".$lkid."&currency=".$currency."&page=".$page."&sign=".$sign;	
+return curlRequest("products/list-promotion-products", $args);
+}
+
+
+
+function listEventCreative($type, $category, $size) {
+
+global $config;
+$str = "";	
+$args = "";		
+ksort($config);	
+foreach ($config as $key => $val)	{
+$str = $str . $key . $val;
+}	
+$sign = strtoupper(md5($api_secret . $str . $api_secret));		
+$args = "api_key=".$api_key."&time=".$time."&lkid="."&type=".$type."&category=".$category."&language=".$language."&size=".$size."&page=".$page."&sign=".$sign."&lkid=".$lkid;	
+return curlRequest("banner/list-event-creative", $args);
+}
+
