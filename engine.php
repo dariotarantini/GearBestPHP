687c5208e580e41fdd79cae61ff939b423496f78
davtur19 <dav.tur19@gmail.com>
2018-01-05 20:46:14 +0100

    Update engine.php

--- engine.php
+++ engine.php
@@ -1,13 +1,13 @@
 <?php
 
-include "config.php";
-
+$file = file_get_contents("./config.json");
+$config = json_decode($file, true);
 $time = time();
 
 function curlRequest($method, $args)
 {
 	$c = curl_init();
-  curl_setopt($c, CURLOPT_HEADER, 0);
+	curl_setopt($c, CURLOPT_HEADER, 0);
 	curl_setopt($c, CURLOPT_URL, "https://affiliate.gearbest.com/api/products/" . $method . "?" . $args);
 	curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
 	$r = curl_exec($c);
@@ -18,7 +18,7 @@ function curlRequest($method, $args)
 
 function getCompletedOrders()
 { 
-  global $config;
+	global $config;
 	$str = "";
 	$args = "";
 	
@@ -27,9 +27,9 @@ function getCompletedOrders()
 	{
 		$str = $str . $key . $val;
 	}
-  $sign = strtoupper(md5($api_secret . $str . $api_secret));
+	$sign = strtoupper(md5($api_secret . $str . $api_secret));
 	
-  $args = "api_key=".$api_key."&time=".$time."&lkid=".$lkid."&currency=".$currency."&page=".$page."&sign=".$sign;
+	$args = "api_key=".$api_key."&time=".$time."&lkid=".$lkid."&currency=".$currency."&page=".$page."&sign=".$sign;
 
-  return curlRequest("list-promotion-products", $args);
+	return curlRequest("list-promotion-products", $args);
 }
