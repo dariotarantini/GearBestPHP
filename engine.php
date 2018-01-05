bda2e1cda183505e22a778eec1b990645c2c519d
davtur19 <dav.tur19@gmail.com>
2018-01-05 22:10:14 +0100

    Update

--- engine.php
+++ engine.php
@@ -14,75 +14,71 @@ function curlRequest($method, $args)
     return json_decode($r);
 }
 
-function getCompletedOrders()
+function getCompletedOrders($page = 1)
 {
     global $config;
+    global $time;
     $str = "";
-    $args = "";
     ksort($config);
     foreach ($config as $key => $val) {
         $str = $str . $key . $val;
     }
     $sign = strtoupper(md5($config['api_secret'] . $str . $config['api_secret']));
-    $args = "api_key=" . $config['api_key'] . "&time=" . $time . "&lkid=" . $config['lkid'] . "&currency=" . $config['currency'] . "&page=" . $config['page'] . "&sign=" . $sign;
+    $args = "api_key=" . $config['api_key'] . "&time=" . $time . "&lkid=" . $config['lkid'] . "&currency=" . $config['currency'] . "&page=" . $page . "&sign=" . $sign;
     return curlRequest("list-promotion-products", $args);
 }
 
-function listPromotionProduct()
+function listPromotionProduct($page = 1)
 {
     global $config;
     $str = "";
-    $args = "";
     ksort($config);
     foreach ($config as $key => $val) {
         $str = $str . $key . $val;
     }
     $sign = strtoupper(md5($config['api_secret'] . $str . $config['api_secret']));
-    $args = "api_key=" . $config['api_key'] . "&time=" . $time . "&lkid=" . $config['lkid'] . "&currency=" . $config['currency'] . "&page=" . $config['page'] . "&sign=" . $sign;
+    $args = "api_key=" . $config['api_key'] . "&time=" . $time . "&lkid=" . $config['lkid'] . "&currency=" . $config['currency'] . "&page=" . $page . "&sign=" . $sign;
     return curlRequest("products/list-promotion-products", $args);
 }
 
 
-function listEventCreative($type, $category, $size)
+function listEventCreative($type, $category, $size, $page = 1)
 {
     global $config;
     $str = "";
-    $args = "";
     ksort($config);
     foreach ($config as $key => $val) {
         $str = $str . $key . $val;
     }
     $sign = strtoupper(md5($config['api_secret'] . $str . $config['api_secret']));
-    $args = "api_key=" . $config['api_key'] . "&time=" . $time . "&lkid=" . "&type=" . $type . "&category=" . $category . "&language=" . $config['language'] . "&size=" . $size . "&page=" . $config['page'] . "&sign=" . $sign . "&lkid=" . $config['lkid'];
+    $args = "api_key=" . $config['api_key'] . "&time=" . $time . "&lkid=" . "&type=" . $type . "&category=" . $category . "&language=" . $config['language'] . "&size=" . $size . "&page=" . $page . "&sign=" . $sign . "&lkid=" . $config['lkid'];
     return curlRequest("banner/list-event-creative", $args);
 }
 
 
-function listProductCreative($type, $category)
+function listProductCreative($type, $category, $page = 1)
 {
     global $config;
     $str = "";
-    $args = "";
     ksort($config);
     foreach ($config as $key => $val) {
         $str = $str . $key . $val;
     }
     $sign = strtoupper(md5($config['api_secret'] . $str . $config['api_secret']));
-    $args = "api_key=" . $config['api_key'] . "&time=" . $time . "&type=" . $type . "&category=" . $category . "&page=" . $config['page'] . "&sign=" . $sign . "&lkid=" . $config['lkid'];
+    $args = "api_key=" . $config['api_key'] . "&time=" . $time . "&type=" . $type . "&category=" . $category . "&page=" . $page . "&sign=" . $sign . "&lkid=" . $config['lkid'];
     return curlRequest("promotions/list-product-creative", $args);
 }
 
 
-function listCoupons($type, $category)
+function listCoupons($type, $category, $page = 1)
 {
     global $config;
     $str = "";
-    $args = "";
     ksort($config);
     foreach ($config as $key => $val) {
         $str = $str . $key . $val;
     }
     $sign = strtoupper(md5($config['api_secret'] . $str . $config['api_secret']));
-    $args = "api_key=" . $config['api_key'] . "&time=" . $time . "&language=" . $config['language'] . "&category=" . $category . "&page=" . $config['page'] . "&sign=" . $sign . "&lkid=" . $config['lkid'];
+    $args = "api_key=" . $config['api_key'] . "&time=" . $time . "&language=" . $config['language'] . "&category=" . $category . "&page=" . $page . "&sign=" . $sign . "&lkid=" . $config['lkid'];
     return curlRequest("coupon/list-coupon", $args);
 }
